6801937260e523ba4c875b3166520f382fdf840e
sono <abdelrhmanstorm123@gmail.com>
2024-12-13 19:43:59 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import Contact from './pages/Contact';
 import Terms from './pages/Terms';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import NotFound from './pages/NotFound';
 
 import { AnimatePresence } from 'framer-motion';
 
@@ -37,7 +36,6 @@ function App() {
               <Route path="/terms" element={<Terms />} />
               <Route path="/signin" element={<SignIn />} />
               <Route path="/signup" element={<SignUp />} />
-              <Route path="*" element={<NotFound />} />
             </Routes>
           </AnimatePresence>
           <Footer />
